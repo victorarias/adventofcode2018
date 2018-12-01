69ff8c88aa3ee002203dba43b47f2ceb87990434
Victor Arias <arias.sp@gmail.com>
2018-12-01 20:33:27 +0100

    Add solution to day 1

---
+++ 01/part1AndPart2.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+const assert = require("assert");
+
+function safeReadFileIntoNumbers(path) {
+  return fs
+    .readFileSync(path, { encoding: "utf-8" })
+    .toString()
+    .trim()
+    .split("\n")
+    .map(i => parseInt(i));
+}
+
+function solvePart1(numbers) {
+  return numbers.reduce((frequency, number) => {
+    return frequency + number;
+  }, 0);
+}
+
+function solvePart2(originalNumbers) {
+  const frequencies = new Set();
+  let numbers = originalNumbers.slice();
+  let currentFrequency = 0;
+
+  do {
+    frequencies.add(currentFrequency);
+    const frequency = numbers.shift();
+    currentFrequency += frequency;
+
+    if (numbers.length === 0) {
+      numbers = originalNumbers.slice();
+    }
+  } while (!frequencies.has(currentFrequency));
+
+  return currentFrequency;
+}
+
+const actualFile = safeReadFileIntoNumbers("./01/data");
+const testFile = safeReadFileIntoNumbers("./01/testFile");
+
+assert(
+  solvePart1(testFile) == 4,
+  `test data failed with ${solvePart1(testFile)}`
+);
+
+assert(
+  solvePart1(actualFile) == 416,
+  `you broke solvePart1 (its returning ${solvePart1(actualFile)})`
+);
+
+assert(
+  solvePart2(testFile) == 10,
+  `test data failed with ${solvePart2(testFile)}`
+);
+
+console.log(solvePart2(actualFile));
